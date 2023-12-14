b5c44945dc9792479289c024d7b9aa510e5928ce
Guillaume Bouyer <guillaume@proxilog.com>
2023-12-14 16:58:39 +0100

    Administration - Modification d'affichage sur la date des articles

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -37,7 +37,7 @@ $articles = $query->fetchAll();
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </head>
     <body>
-        <nav class="navbar bg-primary" data-bs-theme="dark">
+        <nav class="navbar bg-primary navbar-expand-lg" data-bs-theme="dark">
             <div class="container-fluid">
                 <a class="navbar-brand" href="dashboard.php">Administration</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -46,7 +46,7 @@ $articles = $query->fetchAll();
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link text-danger" href="logout.php">Déconnexion</a>
+                            <a class="nav-link text-warning" href="logout.php">Déconnexion</a>
                         </li>
                     </ul>
                 </div>
@@ -71,10 +71,15 @@ $articles = $query->fetchAll();
                             <td><?php echo $article['id']; ?></td>
                             <td><?php echo $article['title']; ?></td>
                             <td><?php echo $article['categories']; ?></td>
-                            <td><?php echo $article['publication_date']; ?></td>
                             <td>
-                                <a href="#">Editer</a> -
-                                <a href="#" class="text-danger">Supprimer</a>
+                                <?php
+                                    $date = DateTime::createFromFormat('Y-m-d H:i:s', $article['publication_date']);
+                                    echo $date->format('d.m.Y');
+                                ?>
+                            </td>
+                            <td>
+                                <a href="#" class="btn btn-light btn-sm">Editer</a>
+                                <a href="#" class="btn btn-danger btn-sm">Supprimer</a>
                             </td>
                         </tr>
                     <?php endforeach; ?>

f6510a496137cb35461f24379900e76ff3a563a2
Guillaume Bouyer <guillaume@proxilog.com>
2023-12-18 14:10:23 +0100

    Affichage des articles

--- index.php
+++ index.php
@@ -32,10 +32,6 @@ foreach ($articles as $key => $article) {
     $groupedArticles[$key]['categories'] = explode(', ', $article['categories']);
 }
 
-echo '<pre>';
-var_dump($groupedArticles);
-echo '</pre>';
-
 ?>
 <!doctype html>
 <html lang="en">
@@ -47,8 +43,55 @@ echo '</pre>';
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     </head>
     <body>
-        <div class="container p-3 pt-5 d-flex justify-content-center align-items-center">
+        <div class="text-center pt-5">
+            <h1>Mon merveilleux blog</h1>
+        </div>
 
+        <div class="container p-3 pt-5 d-flex flex-column justify-content-center">
+            <?php foreach($groupedArticles as $article): ?>
+                <article class="pb-5">
+                    <!-- Titre de l'article -->
+                    <h1><?php echo $article['title']; ?></h1>
+
+                    <!-- Informations sur l'article -->
+                    <small class="d-block text-secondary pb-2">
+                        <?php
+                            $createdAt = DateTime::createFromFormat('Y-m-d H:i:s', $article['publication_date']);
+                            $date = $createdAt->format('d.m.Y');
+                        ?>
+                        Auteur : <?php echo $article['author']; ?> - Posté <?php echo $date; ?>
+                    </small>
+
+                    <!-- Image de couverture -->
+                    <?php if (file_exists("uploads/{$article['cover']}")): ?>
+                        <img
+                            src="<?php echo "uploads/{$article['cover']}" ?>"
+                            alt="<?php echo $article['title']; ?>"
+                            class="img-fluid rounded"
+                        >
+                    <?php endif; ?>
+
+                    <!-- Catégories de l'article -->
+                    <ul class="py-2 list-unstyled d-flex gap-2">
+                        <?php foreach ($article['categories'] as $category): ?>
+                            <li>
+                                <a href="#">
+                                    <span class="badge rounded-pill text-bg-light">
+                                        <?php echo $category ?>
+                                    </span>
+                                </a>
+                            </li>
+                        <?php endforeach; ?>
+                    </ul>
+
+                    <!-- Contenu tronqué de l'article -->
+                    <p><?php echo mb_strimwidth($article['content'], 0, 100, '...'); ?></p>
+
+                    <a href="article.php?id=<?php echo $article['id']; ?>" class="btn btn-sm btn-primary">
+                        Lire la suite...
+                    </a>
+                </article>
+            <?php endforeach; ?>
         </div>
     </body>
 </html>
